73e536dda65faaf86fcd0f050e30db4875530a4b
Asiern <asnulorri@gmail.com>
2022-05-09 13:33:56 +0200

    Util2

--- Util2/main.c
+++ Util2/main.c
@@ -1,25 +1,34 @@
-#include <dirent.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
     if (argc != 2)
     {
-        printf("[+] Uso: ./util2 directorio\n");
-        return 1;
+        fprintf(stderr, "[-] Argumentos incorrectos\n");
+        fprintf(stderr, "[+] Uso: ./util2 directorio\n");
+        exit(1);
     }
 
-    // Abrir directorio
-    DIR *dir = opendir(argv[1]);
+    DIR *dir;
     struct dirent *entry;
     char path[1025];
+    struct stat file_data;
+    mode_t mode;
+    int success;
 
-    if (dir == NULL)
+    // Abrir directorio
+    if ((dir = opendir(argv[1])) == NULL)
     {
-        printf("No se puede abrir el directorio '%s'\n", argv[1]);
-        return 1;
+        fprintf(stderr, "No se puede abrir el directorio '%s'\n", argv[1]);
+        exit(2);
     }
 
     // Iterar elementos
@@ -27,30 +36,48 @@ int main(int argc, char const *argv[])
     {
         if (entry->d_name[0] != '.')
         {
-
-            strcpy(path, argv[1]);
-            strcat(path, "/");
-            strcat(path, entry->d_name);
-
-            mode_t mode;
-
-            switch (entry->d_type)
+            sprintf(path, "%s/%s", argv[1], entry->d_name);
+            if (lstat(path, &file_data) == -1)
             {
-            case DT_REG:
-                mode = "0600";
-
-                break;
-            case DT_DIR:
-                break;
-            case DT_FIFO:
-                break;
-            default:
-                printf("[-] [%s] Tipo de archivo no soportad\n", entry->d_name);
-                continue;
+                fprintf(stderr, "Error al hacer stat al fichero %s\n", entry->d_name);
+                exit(3);
             }
-            chmod(path, mode);
+
+            // Directorios
+            if (S_ISDIR(file_data.st_mode))
+            {
+                mode = file_data.st_mode & 07777;
+                mode |= (S_IXUSR);
+                mode &= ~(0050);
+                if ((success = chmod(path, mode)) == -1)
+                {
+                    fprintf(stderr, "Error al cambiar permisos del directorio %s\n", entry->d_name);
+                    exit(4);
+                }
+                else if (S_ISLNK(file_data.st_mode)) // Symbolik link
+                {
+                    mode = file_data.st_mode & 07777;
+                    mode |= (0700);
+                    mode &= ~(0077);
+                    if ((success = chmod(path, mode)) == -1)
+                    {
+                        fprintf(stderr, "Error al cambiar permisos del fichero %s\n", entry->d_name);
+                        exit(5);
+                    }
+                }
+                else
+                {
+                    mode = file_data.st_mode & 07777;
+                    mode &= ~(0077);
+                    if ((success = chmod(path, mode)) == -1)
+                    {
+                        fprintf(stderr, "Error al cambiar permisos del fichero %s\n", entry->d_name);
+                        exit(6);
+                    }
+                }
+            }
         }
     }
-
+    closedir(dir);
     return 0;
 }
